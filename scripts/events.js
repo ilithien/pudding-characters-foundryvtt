2b0d15be6480ef5f0c099bbb798c5a288f94f4ed
Oscar Palerm <darktails@gmail.com>
2025-02-03 20:19:31 +0100

    Register settings

--- scripts/events.js
+++ scripts/events.js
@@ -1,3 +1,8 @@
+import { registerSettings } from "./settings";
+
+Hooks.once("init", async function () {
+  registerSettings();
+});
 
 Hooks.on("updateActor", function (actor, updatedPartial) {
   fetch(`https://pudding.obitur.com/actor/${actor._id}`, {
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,27 +1,29 @@
 import { MODULE_NAME } from "./constants";
 
-game.settings.register(MODULE_NAME, "clientId", {
-  name: "API Client ID",
-  hint: "A valid client ID to update the actors",
-  scope: "world",
-  config: true,
-  type: new foundry.data.fields.StringField({
-    required: true,
-    blank: false,
-    initial: "default",
-  }),
-  requiresReload: true,
-});
+export function registerSettings() {
+  game.settings.register(MODULE_NAME, "clientId", {
+    name: "API Client ID",
+    hint: "A valid client ID to update the actors",
+    scope: "world",
+    config: true,
+    type: new foundry.data.fields.StringField({
+      required: true,
+      blank: false,
+      initial: "default",
+    }),
+    requiresReload: true,
+  });
 
-game.settings.register(MODULE_NAME, "clientId", {
-  name: "API Client Key",
-  hint: "API Client key",
-  scope: "world",
-  config: true,
-  type: new foundry.data.fields.StringField({
-    required: true,
-    blank: false,
-    initial: "default",
-  }),
-  requiresReload: true,
-});
+  game.settings.register(MODULE_NAME, "clientId", {
+    name: "API Client Key",
+    hint: "API Client key",
+    scope: "world",
+    config: true,
+    type: new foundry.data.fields.StringField({
+      required: true,
+      blank: false,
+      initial: "default",
+    }),
+    requiresReload: true,
+  });
+}
