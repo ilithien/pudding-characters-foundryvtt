6e440284026b913339c59dfcc71d08e059d4d572
Oscar Palerm <darktails@gmail.com>
2025-02-03 20:30:53 +0100

    chore(0.1.1) Relase 0.1.1

--- scripts/events.js
+++ scripts/events.js
@@ -1,9 +1,13 @@
 import { registerSettings } from "./settings";
 
 Hooks.once("init", async function () {
+  console.log(
+    "Pudding Character | Loading Pudding Character for FoundryVTT..."
+  );
   registerSettings();
 });
 
+console.log(" Pudding Character | Registering 'updateActor' hook");
 Hooks.on("updateActor", function (actor, updatedPartial) {
   fetch(`https://pudding.obitur.com/actor/${actor._id}`, {
     method: "POST",
